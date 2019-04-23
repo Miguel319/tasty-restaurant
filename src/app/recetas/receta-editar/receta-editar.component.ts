3905ec6ed9855591c4b7b6af853601d4891bc605
Miguel319 <miguelp90.319@gmail.com>
2019-04-23 15:55:57 -0400

    Enrutamiento y correción de errores en los servicios

---
+++ src/app/recetas/receta-editar/receta-editar.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+
+@Component({
+  selector: 'app-receta-editar',
+  templateUrl: './receta-editar.component.html',
+  styleUrls: ['./receta-editar.component.css']
+})
+export class RecetaEditarComponent implements OnInit {
+  id;
+  number;
+  modoEditar = false;
+
+  constructor(private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['id'];
+          this.modoEditar = params['id'] != null;
+          console.log(this.modoEditar);
+        }
+      );
+  }
+}
